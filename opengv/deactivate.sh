87e3212b4a12abc3632fdf61d783f68e7e539bc0
Sudeep Pillai <spillai@csail.mit.edu>
2016-10-08 16:00:05 -0400

    added ld library paths as well

--- opengv/deactivate.sh
+++ opengv/deactivate.sh
@@ -1,5 +1,7 @@
 echo "--------------------------------------------------------------"
 echo "Deactivating pygtsam: $CONDA_PREFIX"
 export PYTHONPATH=$_PYOPENGV_PYTHONPATH_BACKUP
+export LD_LIBRARY_PATH=$_PYOPENGV_LD_LIBRARY_PATH_BACKUP
 unset _PYOPENGV_PYTHONPATH_BACKUP
+unset _PYOPENGV_LD_LIBRARY_PATH_BACKUP
 echo "--------------------------------------------------------------"
